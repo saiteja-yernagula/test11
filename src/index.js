7dea28ef4eec6bd39a97d9ad8cc681a37833cf12
saiteja-yernagula <saiteja.yernagula@gmail.com>
2023-07-08 19:15:09 +0530

    teee

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Counter from './Counter';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <Counter/>
+    
     
   </React.StrictMode>
 );
