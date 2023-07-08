2a90bbfcc4412769a8368238ee498df4c2048bd1
saiteja-yernagula <saiteja.yernagula@gmail.com>
2023-07-08 20:27:58 +0530

    zvdz

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ function App(){
          <Routes>
             <Route path='/' element={<Newcom/>}/>
             <Route path='/todo' element={<Todo/>}/>
+
          
          </Routes>
     </BrowserRouter>
