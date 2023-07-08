7cb35367ffa71c87436f51848985e9374d808c14
saiteja-yernagula <saiteja.yernagula@gmail.com>
2023-07-08 19:56:20 +0530

    cgn

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Todo from './Todo';
 import Newcom from './Newcom';
 
-import Pixabay from './Pixabay';
-import Search from './Search';
 
 
 
@@ -25,8 +23,7 @@ function App(){
          <Routes>
             <Route path='/' element={<Newcom/>}/>
             <Route path='/todo' element={<Todo/>}/>
-            <Route path='/gallery' element={<Pixabay/>}/>
-            <Route path='/gallery/search/:gdata' element={<Search/>}/>
+            
 
 
             
